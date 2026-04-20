7c0e7641a53ac5388e9770ed2130199142c1e6d0
79akhil105-tech <79akhil105@gmail.com>
2026-04-20 15:35:44 +0400

    Delete App.jsx

--- App.jsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <div style={{ backgroundColor: '#000', color: '#fff', height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-      <h1 style={{ letterSpacing: '4px', textTransform: 'uppercase' }}>Cyber Noir Room: System Initialized</h1>
-    </div>
-  )
-}
-
-export default App
